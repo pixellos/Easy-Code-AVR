dc074410638f477538f215fc5b79d6cc955449a9
Mateusz Popielarz <rogoznocraft@gmail.com>
2016-03-03 21:48:50 +0100

    Moved into right folders

--- Avr5/Test 12 02/MotorServoController/MotorServoController/AVR5/Helpers/Screen.h
+++
@@ -1,7 +0,0 @@
-#ifndef INCFILE1_H_
-#define INCFILE1_H_
-
-void str_t_bfr_cpy(uint8_t x, char *c);
-
-#include "Screen.c"
-#endif /* INCFILE1_H_ */
--- Avr5/Test 12 02/MotorServoController/MotorServoController/fonts.c
+++
@@ -1,7 +0,0 @@
-*
- * fonts.c
- *  Created on: 4 lip 2015
- *      Author: Piter
- */
-#include <avr/pgmspace.h>
-#include "fonts.h"
